98f08fb28dc4ad106b447937cbea963504b7d42f
ydgu589 <52509459+ydgu589@users.noreply.github.com>
2019-08-10 19:54:33 +0100

    Update index1.js

--- index1.js
+++ index1.js
@@ -1,22 +1,7 @@
-  
-
-
-
 if(typeof(width)=="undefined"){
-
 var width = '100%';
-
 }
-
 if(typeof(height)=="undefined"){
-
 var height = '100%';
-
 }
-
 document.write('<iframe allowfullscreen width="'+width+'" height="'+height+'" scrolling="no" frameborder="0" marginwidth="0" marginheight="0" scrolling="no" allowtransparency="true" src="http://iptv-janberyan.com:25461/live/00320001140141/00320001140141/'+C+'.m3u8"></iframe>');
-
-
-
-   
-
